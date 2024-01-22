f8966b3f88dda8aafb177cdd117b95114437c367
Larissa Faria <lari.f4ria@gmail.com>
2024-01-22 13:03:02 -0300

    Criação de server Json, interface para tipos, e injeção de dependências

--- memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ memoteca/src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,13 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'alo alo',
-      autoria:'uauauauau',
-      modelo: 'modelo2'
-    }
-  ]
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
