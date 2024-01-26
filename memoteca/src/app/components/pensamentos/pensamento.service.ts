a493e263759abfca7e3adcf0a111522e249a7e4b
Larissa Faria <lari.f4ria@gmail.com>
2024-01-26 16:44:04 -0300

    Melhora fluxo entre páginas, lista de favoritos, muda título

--- memoteca/src/app/components/pensamentos/pensamento.service.ts
+++ memoteca/src/app/components/pensamentos/pensamento.service.ts
@@ -12,20 +12,25 @@ export class PensamentoService {
 
   constructor(private http: HttpClient) { }
 
-  listar(pagina : number, filtro: string): Observable<Pensamento[]> {
+  listar(pagina : number, filtro: string, favoritos: boolean): Observable<Pensamento[]> {
 
     const itensPorPagina = 6;
 
     let parametros = new HttpParams()
       .set("_page", pagina)
-      .set("_limit", itensPorPagina);
+      .set("_limit", itensPorPagina)
 
       if(filtro.trim().length > 2){
         parametros = parametros.set("q", filtro)
       }
 
+      if(favoritos){
+        parametros = parametros.set("favorito", true)
+      }
+
     return this.http.get<Pensamento[]>(this.API, {params: parametros})
   }
+
 
   criar(pensamento: Pensamento): Observable<Pensamento> {
     return this.http.post<Pensamento>(this.API, pensamento)
